8be3e28755d17f22f661dfc29db2f0c343c2cdb8
sakshamsingh07 <singhsaksham8235@gmail.com>
2022-12-01 04:44:24 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sum
+{
+    public static void main(String [] args)
+    {
+        int a=5;
+        int b=10;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
