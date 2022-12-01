4b3f29cb8b349b2e29368dc132bca7c6551bf54f
sakshamsingh07 <singhsaksham8235@gmail.com>
2022-12-01 13:08:51 +0530

    all code

--- sub.java
+++ sub.java
@@ -1,3 +1,12 @@
-public class sub {
+import java .util.*;
+public class sub 
+{
+    public static void main (String []args)
+    {
+        int a =10;
+        int b =5;
+        int c=a-b;
+        System.out.println(c);
+    }
     
 }
